edb8d5033b6b33c48165b56b62d4801f98bdf13f
rafaelfang <cf797@mail.missouri.edu>
2014-09-20 09:36:56 -0500

    JavaHDFSApi

--- JavaHDFSApi/src/main/java/edu/missouri/hadoop/App.java
+++ JavaHDFSApi/src/main/java/edu/missouri/hadoop/App.java
@@ -1,13 +1,36 @@
 package edu.missouri.hadoop;
 
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.Path;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	private static final String message = "My First Hadoop API call!";
+
+	public static void main(String[] args) throws IOException {
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.get(conf);
+
+		Path filenamePath = new Path("hadoop.txt");
+		if (fs.exists(filenamePath)) {
+			fs.delete(filenamePath, true);
+		}
+
+		FSDataOutputStream out = fs.create(filenamePath);
+		out.writeUTF(message);
+		out.close();
+
+		FSDataInputStream in = fs.open(filenamePath);
+		String messageIn = in.readUTF();
+		System.out.println(messageIn);
+		in.close();
+	}
 }
